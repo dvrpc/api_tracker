c4daecb4c549294b32021de8dd11c17411666df3
Kris Warner <kdwarn@protonmail.com>
2022-09-16 15:37:06 -0400

    Fix paths of shell script
    This is really only used on the server, but trying to get into version
    control nonetheless.

--- crawler/crawl.sh
+++ crawler/crawl.sh
@@ -1,6 +1,4 @@
 #!/bin/bash                                         
 
-# activate the virtual environment and then run the script
-source /srv/api/api_tracker/ve/bin/activate
-
-python /srv/api/api_tracker/crawler/crawler.py
+cd /srv/api/api_tracker/crawler
+/srv/api/api_tracker/ve/bin/python crawler.py
